5662787a3319c3bc77723e9119f5389f24ad5c47
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2021-03-07 19:23:00 -0600

    Change implementation to an REST api

---
+++ t-mobile/interview/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/calculate', (req, res) => {
+    try{
+        var input = req.body.input.split(" ");
+        var output = postFix(input);
+        var result = {output: output.toString()}
+        res.json(result);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+});
+
+app.listen(port, () => console.log(`Postfix app listening on port ${port}!`));
+
+ function postFix(value){
+     // take the last element of the array coming in since that is the operand
+     let stack = [];
+     let result = 0
+     for(let i = 0; i < value.length; i++) {
+         if(value[i] === "+" || value[i] === "-" || value[i] === "*" || value[i] === "/" ){
+
+             let num2 = stack.pop();
+             let num1 = stack.pop();
+
+             if(value[i] === '+'){
+                result = Number(num1) + Number(num2);
+                stack.push(result)
+            }
+            else if(value[i] === '-'){
+                result = Number(num1) - Number(num2);
+                stack.push(result)
+            }
+            else if(value[i] === '*'){
+                result = Number(num1) * Number(num2);
+                stack.push(result)
+            }
+            else{
+                result = Number(num1) / Number(num2);
+                stack.push(result)
+            } 
+         }
+         else{
+            stack.push(value[i]);
+                    
+         }
+     }
+     return stack.pop();
+  }
+
+  module.exports = postFix
---
+++ t-mobile/interview/index.test.js
@@ -0,0 +1,43 @@
+const postFix = require('./index');
+
+test('should add correctly', ()=> {
+    const input = ["4.5", "5.25", "+"]
+    const output = 9.75;
+
+    expect(postFix(input)).toEqual(output);
+})
+
+test('should subtract correctly', ()=> {
+    const input = ["4.5", "5.25", "-"]
+    const output = -0.75;
+
+    expect(postFix(input)).toEqual(output);
+})
+
+test('should multiply correctly', ()=> {
+    const input = ["8", "6.5", "*"]
+    const output = 52;
+
+    expect(postFix(input)).toEqual(output);
+})
+
+test('should divide correctly', ()=> {
+    const input = ["50", "2", "/"]
+    const output = 25;
+
+    expect(postFix(input)).toEqual(output);
+})
+
+test('should work with multiple operands', ()=> {
+    const input = ["3.5", "2", "+", "4.25", "4", "*", "-"]
+    const output = -11.5;
+
+    expect(postFix(input)).toEqual(output);
+})
+test('should work with multiple operands 2', ()=> {
+    const input = ["4", "2", "*", "5", "2", "*", "+"]
+    const output = 18;
+
+    expect(postFix(input)).toEqual(output);
+})
+
