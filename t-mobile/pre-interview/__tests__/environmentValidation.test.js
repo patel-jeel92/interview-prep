067c8ddda5158c2d9f1a92ae27d820a1cffd399b
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2022-02-20 11:14:55 -0500

    Remove unwanted files

--- t-mobile/pre-interview/__tests__/environmentValidation.test.js
+++
@@ -1,5 +0,0 @@
-import allCaps from "../src/environmentValidation";
-
-test("Should return all caps of the string", () => {
-  expect(allCaps("hello world")).toBe("HELLO WORLD");
-});
--- t-mobile/pre-interview/src/environmentValidation.js
+++
@@ -1,3 +0,0 @@
-export default function allCaps(string) {
-  return string.toUpperCase();
-}
